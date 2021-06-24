c8d915e4a58edaa4ef90d5432af30e738dc7f30d
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2021-06-24 14:29:37 +0300

    owl resp

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
 // For owl-carousel
-jQuery(document).ready(($)=>{
+$(document).ready(($)=>{
     $('.element-1').owlCarousel({
         nav: true,
         items: 3,
         loop: true,
         margin: 23,
         dots: false,
+        // responsiveClass: true,
+        // responsive: {
+        //     0:{
+        //       items: 1
+        //     },
+        //     640:{
+        //       items: 2
+        //     },
+        //     840:{
+        //       items: 3
+        //     }
+        // }
     });
-    
 });
+ 
