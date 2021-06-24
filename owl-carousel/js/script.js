cc8b9f18d6b76b6ec367ad0755e769836bad19e8
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2021-06-24 14:38:52 +0300

    add lazy load

--- owl-carousel/js/script.js
+++ owl-carousel/js/script.js
@@ -1,12 +1,22 @@
-jQuery(document).ready(($)=>{
-
-    // документ загружен!
-
+$(document).ready(($)=>{
     $('.element-1').owlCarousel({
         nav: true,
         items: 3,
         loop: true,
-        margin: 20
+        margin: 23,
+        lazyLoad: true,
+        responsiveClass: true,
+        responsive: {
+            0:{
+              items: 1
+            },
+            710:{
+              items: 2
+            },
+            840:{
+              items: 3
+            }
+        }
     });
 
 });
